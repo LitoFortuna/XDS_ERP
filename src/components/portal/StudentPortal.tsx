f103f4e421da9d93158372e9a737b7d760b07638
LitoFortuna <raulfdz3@gmail.com>
2026-01-27 06:33:07 +0100

    style: eliminar emoji duplicado en sección Mis Datos Personales

--- src/components/portal/StudentPortal.tsx
+++ src/components/portal/StudentPortal.tsx
@@ -216,7 +216,7 @@ const StudentPortal: React.FC<StudentPortalProps> = ({ student, onLogout }) => {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2 text-blue-400" viewBox="0 0 20 20" fill="currentColor">
                                         <path fillRule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clipRule="evenodd" />
                                     </svg>
-                                    👤 Mis Datos Personales
+                                    Mis Datos Personales
                                 </h3>
                                 <button
                                     onClick={() => setShowChangeRequestModal(true)}
